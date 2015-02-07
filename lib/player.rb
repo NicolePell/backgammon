cd7ffc4102822d02710ca7a6e3de95f3ca854609
NicolePell <pellicena@gmail.com>
2015-02-07 19:33:22 +0000

    player can hold checkers

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,16 @@
+require_relative 'checker'
+
 class Player
 
-  attr_reader :name
+  attr_reader :name, :checkers
 
   def initialize(name)
     @name = name
+    @checkers = []
+  end
+
+  def start_checkers(checker)
+    15.times { @checkers << checker }
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,11 +2,26 @@ require 'player'
 
 describe Player do
 
+
   let(:player) { Player.new('Naruto') }
+  let(:red) { Checker.new(:red) }
 
   context 'when initializing' do
     it 'has a name' do
       expect(player.name).to eq 'Naruto'
     end
   end
+
+  context 'when starting game' do
+    it 'can hold 15 checkers' do
+      player.start_checkers(red)
+      expect(player.checkers.count).to eq 15
+    end
+
+    it 'knows what colour checkers it has' do
+      player.start_checkers(red)
+      expect(player.checkers[0].colour).to eq :red
+    end
+  end
+
 end
