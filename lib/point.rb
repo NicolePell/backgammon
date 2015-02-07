25896c9f7e7f8a4d0f2b150850353710e59a6c71
NicolePell <pellicena@gmail.com>
2015-02-07 14:58:44 +0000

    a point can hold checkers

--- lib/point.rb
+++ lib/point.rb
@@ -1,12 +1,14 @@
 class Point
 
-  attr_reader :status
+  attr_reader :status, :checkers
 
   def initialize(status=:empty)
     @status = status
+    @checkers = []
   end
 
   def place(checker)
+    checkers << checker
     @status = :occupied
   end
 
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -11,10 +11,15 @@ describe Point do
     end
   end
 
-  context 'checkers placed on point' do
-    it 'a checker can move to an empty point' do
+  context 'when checkers are placed on a point' do
+    it "it's status changes from empty to occupied" do
       point.place(red)
       expect(point.status).to be :occupied
+    end
+
+    it 'it can hold the checker' do
+      point.place(red)
+      expect(point.checkers.length).to eq 1
     end
   end
 
